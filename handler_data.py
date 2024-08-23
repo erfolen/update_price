bbc8a88f8ae77a1e784f40398b95938f2dad9adf
videbul <videbul@gmail.com>
2024-08-23 08:15:31 +0300

    change choose columns

--- handler_data.py
+++ handler_data.py
@@ -8,15 +8,20 @@ class HandlerData:
         self.columns = []
 
     def get_df(self):
-        self.columns.append(self.get_columns('Артикул'))
-        self.columns.append(self.get_columns('ИнтернетМагазин'))
-        self.columns.append(self.get_columns('Остаток'))
-        print(self.columns)
-        df = self.df.iloc[:, [14, 17, 19]].dropna()
+        self.columns.append(self.get_column('Артикул'))
+        self.columns.append(self.get_column('ИнтернетМагазин'))
+        self.columns.append(self.get_column('Остаток'))
+        df = self.df.iloc[:, self.columns].dropna()
+        df.columns = ['_MODEL_', '_PRICE_', '_QUANTITY_']
+        data_new_columns = {'_SORT_ORDER_': 110, '_STOCK_STATUS_': 'В наличии'}
+        self.add_columns(df, data_new_columns)
         return df
 
-    def get_columns(self, name_data):
+    def get_column(self, name_data):
         s = self.df.isin([name_data]).any(axis=0)
         name_columns = s.loc[s].index.to_list()[0]
-        return re.sub('\D', '', name_columns)
+        return int(re.sub('\D', '', name_columns))
 
+    def add_columns(self, df, data):
+        for column, value in data.items():
+            df[column] = value
